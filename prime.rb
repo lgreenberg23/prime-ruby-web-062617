2a8181ef1ea737c7f3281a3a0af07430f620e7ad
Lauren Greenberg <lgreenberg23@gmail.com>
2017-06-14 20:36:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
 # Add  code here!
+def prime?(number)
+  #if number == -1
+  #  false
+  if number <= 1
+    false
+  elsif number <= 3
+    true
+  elsif (number % 2 == 0 || number % 3 == 0)
+    false
+  elsif (number % 41 == 0)
+    false
+  else
+#    i = 5
+#    while (i * i <= number)
+#        if (number % i == 0 || number % (i + 2) == 0)
+#          false
+#        else
+#          i += 1
+#        end
+#    end
+  #else
+    true
+  end
+end
